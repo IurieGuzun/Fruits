88fc9b66160fa8e0bbec5f9065bd0ac4ca5bf0f6
Iurie Guzun <iurie_guzun@yahoo.com>
2022-03-05 15:38:43 -0500

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Iurie Guzun on 2022-03-05.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
